4b3c80bd1273899ef11b21c2489f40ee11746091
Pio Concepcion <pioconcepcion3@yahoo.com>
2024-08-30 11:36:23 +0800

    fixed how coordinates are presented

--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,23 @@ function App() {
 
 	const [ board, setBoard ] = useState( initialBoard )
 
-	console.log('render')
-
-	return (
-		<div className='chessboard'>
-		  {board.map( ( row, i ) => {
-			return ( 
-			  <div 
-				key={i} className={ (i+1) % 2 === 0 ? 'chessboard-row even' : 'chessboard-row odd' } 
-			  >
-			  { row.map( ( square, j ) => {
-				return (
-				<Tile
-				  key={j}
-				  coordinate={j+i.toString(10)}
-				  board={board}
-				  setBoard={setBoard}
-				> 
-				  { square ? <Piece name={square}/> : null }
-				</Tile> )
-			  } ) }
-			</div> )
-		  } )}
-		</div>
-		)
+	return ( <div className='chessboard'>
+                { board.map( ( row, i ) => {
+                    return ( <div key={i} className={ (i+1) % 2 === 0 ? 'chessboard-row even' : 'chessboard-row odd' } >
+                            { row.map( ( square, j ) => {
+                                return (
+                                <Tile
+                                    key={j}
+                                    coordinate={i+''+j}
+                                    board={board}
+                                    setBoard={setBoard}
+                                > 
+                                    { square ? <Piece side={ square.slice( 0, 5 ) } name={ square } /> : null }
+                                </Tile> )
+                            } ) }
+                        </div> )
+                } ) }
+        </div> )
 }
 
 export default App
